82c7b4b5690872db39a237d5482ba2bce7023ad8
ryanrapini <ryanrapini@gmail.com>
2015-01-17 04:13:37 -0500

    Initial Commit

---
+++ ColorPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorPicker
+//
+//  Created by Ryan Rapini on 1/17/15.
+//  Copyright (c) 2015 betaDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ColorPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorPicker
+//
+//  Created by Ryan Rapini on 1/17/15.
+//  Copyright (c) 2015 betaDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
